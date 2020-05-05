2f4464c6598d20134fdd69490f9815ec4ebd87e2
Арсентьев Антон <arsentiev@mo.nept.ru>
2020-05-05 19:17:59 +0300

    rthuytyj

--- main.py
+++ main.py
@@ -1,51 +1,29 @@
 import os
 from flask import Flask
 from flask import request
+from telegram import Update
 from bottools.bot import Bot
 from bottools.constants import *
 from postgrestools import commands
 # from argparse import ArgumentParser
 from loggingtools.register import Logger
 from postgrestools.postgres import Postgres
-from telegram import (
-    Update
-)
-# from telegram.ext import (
-#     CommandHandler, Dispatcher
-# )
-# from queue import Queue
 
 server = Flask(__name__)
 
-logger = Logger(commands.LOGGER_APP_VALUE)
-
-postgres = Postgres(host=commands.HOST_APP_VALUE,
-                    port=commands.PORT_APP_VALUE,
-                    database=commands.DATABASE_APP_VALUE,
-                    user=commands.USER_APP_VALUE,
-                    password=commands.PASSWORD_APP_VALUE,
-                    connection_type=commands.CONNECTION_TYPE_DROP_AND_CREATE,
-                    logger=logger)
+# logger = Logger(commands.LOGGER_APP_VALUE)
+#
+# postgres = Postgres(host=commands.HOST_APP_VALUE,
+#                     port=commands.PORT_APP_VALUE,
+#                     database=commands.DATABASE_APP_VALUE,
+#                     user=commands.USER_APP_VALUE,
+#                     password=commands.PASSWORD_APP_VALUE,
+#                     connection_type=commands.CONNECTION_TYPE_DROP_AND_CREATE,
+#                     logger=logger)
 
 # if postgres.is_connected():
-bot = Bot(logger=logger, postgres=postgres)
-bot.start_pooling()
-
-# bot = Bot(token=BOT_TOKEN)
-# bot.setWebhook(HEROKU_APP_URL + BOT_TOKEN)
-# update_queue = Queue()
-# dp = Dispatcher(bot, update_queue, use_context=True)
-#
-#
-# def start(update, context):
-#     context.bot.send_message(
-#         chat_id=update.effective_chat.id,
-#         text="_команда старт_"
-#     )
-
-
-# start_handler = CommandHandler('start', start)
-# dp.add_handler(start_handler)
+# bot = Bot(logger=logger, postgres=postgres)
+# bot.start_pooling()
 
 # def parse_args():
 #     parser = ArgumentParser()
@@ -90,17 +68,17 @@ if __name__ == "__main__":
     #                     connection_type=parse_args_namespace.postgres_type,
     #                     logger=logger)
 
-    # logger = Logger(commands.LOGGER_APP_VALUE)
-    #
-    # postgres = Postgres(host=commands.HOST_APP_VALUE,
-    #                     port=commands.PORT_APP_VALUE,
-    #                     database=commands.DATABASE_APP_VALUE,
-    #                     user=commands.USER_APP_VALUE,
-    #                     password=commands.PASSWORD_APP_VALUE,
-    #                     connection_type=commands.CONNECTION_TYPE_DROP_AND_CREATE,
-    #                     logger=logger)
-    #
-    # if postgres.is_connected():
-    #     bot = Bot(logger=logger, postgres=postgres)
-    #     bot.start_pooling()
+    logger = Logger(commands.LOGGER_APP_VALUE)
+
+    postgres = Postgres(host=commands.HOST_APP_VALUE,
+                        port=commands.PORT_APP_VALUE,
+                        database=commands.DATABASE_APP_VALUE,
+                        user=commands.USER_APP_VALUE,
+                        password=commands.PASSWORD_APP_VALUE,
+                        connection_type=commands.CONNECTION_TYPE_DROP_AND_CREATE,
+                        logger=logger)
+
+    if postgres.is_connected():
+        bot = Bot(logger=logger, postgres=postgres)
+        bot.start_pooling()
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
